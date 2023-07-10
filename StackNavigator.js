450e31cb0424483e5e9fe0e1a71848480d4bfffe
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2023-07-10 10:00:43 +0600

    order section update

--- StackNavigator.js
+++ StackNavigator.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PickUpScreen from './screens/PickUpScreen';
 import CartScreen from './screens/CartScreen';
+import OrderScreen from './screens/OrderScreen';
+
 
 export default function StackNavigator() {
     const Stack = createNativeStackNavigator();
@@ -14,6 +16,7 @@ export default function StackNavigator() {
       <Stack.Screen name="Home" component={HomeScreen}  options={{headShown:false}}/>
       <Stack.Screen name="PicUp" component={PickUpScreen}  options={{headShown:false}}/>
       <Stack.Screen name="Cart" component={CartScreen}  options={{headShown:false}}/>
+      <Stack.Screen name="Order" component={OrderScreen}  options={{headShown:false}}/>
     </Stack.Navigator>
   </NavigationContainer>
   )
